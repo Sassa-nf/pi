552dd840de8bd9f8ed8298198637faceb052bd9c
Sassa NF <sassa.nf@gmail.com>
2020-06-29 12:20:19 +0100

    Intro the problem

--- bit_vs_hare/src/main.rs
+++ bit_vs_hare/src/main.rs
@@ -1,3 +1,45 @@
+/*
+ * Given an array nums containing n + 1 integers where each integer is between
+ * 1 and n (inclusive), prove that at least one duplicate number must exist.
+ * Assume that there is only one duplicate number, find the duplicate one.
+ *
+ * You must not modify the array (assume the array is read only).
+ * You must use only constant, O(1) extra space.
+ * Your runtime complexity should be less than O(n^2).
+ * There is only one duplicate number in the array, but it could be repeated
+ * more than once.
+ *
+ * Input: [1,3,4,2,2]
+ * Output: 2
+ *
+ * Input: [3,1,3,4,2]
+ * Output: 3
+ *
+ * The discussion of the problem points out one can represent this as a
+ * problem of finding a loop in a graph. Then arranging two arrays:
+ *
+ * [0, 1, 2, 3, 4] - indices
+ * [1, 3, 4, 2, 2] - actual input
+ *
+ * lends itself to the interpretation that this is a linked list 0->1, 1->3,
+ * 2->4, 3->2, 4->2.
+ *
+ * Since 0 is not used, then xs[0] is definitely not part of a loop. Also,
+ * since all numbers are between 1 and n, then the loop definitely is entered.
+ * (All indices are reachable from 0).
+ *
+ * Thus we can use the tortoise-and-hare algorithm to find the loop, and thus
+ * the repetition. It can be shown to be O(n)
+ *
+ * However, that's not the only way to work out the loop in O(n). Bit counting
+ * also works. It is guaranteed to work in less than 32 re-scans of the array, but
+ * this is much faster than the tortoise-and-hare algorithm, because it is just
+ * a scan of numbers, not interpretation of them as indices. The maths is easy
+ * to vectorize, and the scan is in cache-efficient order, not random.
+ *
+ * For the array of 64K entries and a very large loop this is 2x faster than the
+ * tortoise-and-hare. (But the same implementation in Python is 100x slower)
+ */
 use std::time::Instant;
 
 fn bit_count(xs: &[u32]) -> u32 {
