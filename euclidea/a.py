77dbf3ba8eb99555015787fa60abe8a55e81156f
Sassa NF <sassa.nf@gmail.com>
2020-10-17 21:06:47 +0100

    ok, we can solve and explain a simple puzzle

--- euclidea/a.py
+++ euclidea/a.py
@@ -4,10 +4,18 @@ def sgn(a):
    return -1 if a < 0 else 1
 
 EPSILON = 0.001
+P_NAME = 0
+L_NAME = 0
+C_NAME = 0
+
 class Point:
-   def __init__(self, x, y):
+   def __init__(self, x, y, proof):
+      global P_NAME
       self.x = x
       self.y = y
+      self.name = 'P_%s' % P_NAME
+      P_NAME += 1
+      self.proof = proof
 
    def __hash__(self):
       return self.x.__hash__() + self.y.__hash__()
@@ -21,6 +29,9 @@ class Point:
       l = sqrt(dx*dx + dy*dy)
 
       return l < EPSILON
+
+   def __str__(self):
+      return '<%s, %s>' % (self.x, self.y)
 
    def intersect(self, shape):
       if type(shape) == Point:
@@ -49,13 +60,18 @@ class Point:
 
 class Line:
    def __init__(self, p1, p2):
-      self.p = Point(p1.x, p1.y)
+      global L_NAME
+      self.p = Point(p1.x, p1.y, p1.proof)
 
       dx = p2.x - p1.x
       dy = p2.y - p1.y
 
       l = sqrt(dx*dx + dy*dy)
-      self.v = Point(dx / l, dy / l)
+      self.v = Point(dx / l, dy / l, '(computed)')
+
+      self.name = 'L_%s' % L_NAME
+      L_NAME += 1
+      self.proof = '%s...%s' % (p1.name, p2.name)
 
    def __hash__(self):
       return self.p.__hash__() + self.v.__hash__()
@@ -71,6 +87,9 @@ class Line:
       # the unit vectors are colinear:
       return abs(self.v.x * l.v.x + self.v.y * l.v.y) >= 1-EPSILON
 
+   def __str__(self):
+      return '[%s...%s]' % (self.p, self.v)
+
    def intersect(self, shape):
       if type(shape) == Point:
          return shape.intersect(self)
@@ -83,13 +102,13 @@ class Line:
             return [self.p]
 
          l = Line(self.p, shape.p)
-         l.v = Point(shape.v.x, shape.v.y)
+         l.v = Point(shape.v.x, shape.v.y, '(computed)')
 
          if self == l: # parallel lines
             return []
 
          t = ((shape.p.x - self.p.x) * self.v.y - (shape.p.y - self.p.y) * self.v.x) / (shape.v.x * self.v.y - shape.v.y * self.v.x)
-         return [Point(shape.p.x + shape.v.x * t, shape.p.y + shape.v.y * t)]
+         return [Point(shape.p.x + shape.v.x * t, shape.p.y + shape.v.y * t, '%s ^ %s' % (self.name, shape.name))]
 
       if type(shape) == Circle:
          #    |x1  y1|
@@ -102,9 +121,9 @@ class Line:
             
          dd = sqrt(discriminant)
          p1 = Point(shape.c.x + det * self.v.y + sgn(self.v.y) * self.v.x * dd,
-                    shape.c.y + (-det) * self.v.x + abs(self.v.y) * dd)
+                    shape.c.y + (-det) * self.v.x + abs(self.v.y) * dd, '%s ^ %s' % (self.name, shape.name))
          p2 = Point(shape.c.x + det * self.v.y - sgn(self.v.y) * self.v.x * dd,
-                    shape.c.y + (-det) * self.v.x - abs(self.v.y) * dd)
+                    shape.c.y + (-det) * self.v.x - abs(self.v.y) * dd, '%s ^ %s' % (self.name, shape.name))
          if p1 == p2:
             return [p1]
          return [p1, p2]
@@ -113,11 +132,15 @@ class Line:
 
 class Circle:
    def __init__(self, c, r):
-      self.c = Point(c.x, c.y)
+      global C_NAME
+      self.c = Point(c.x, c.y, c.proof)
       dx = r.x - c.x
       dy = r.y - c.y
 
       self.r = sqrt(dx*dx + dy*dy)
+      self.name = 'C_%s' % C_NAME
+      C_NAME += 1
+      self.proof = '%s -> %s' % (c.name, r.name)
 
    def __hash__(self):
       return self.c.__hash__() + self.r.__hash__()
@@ -127,6 +150,9 @@ class Circle:
          return False
 
       return abs(self.r - c.r) < EPSILON and self.c == c.c
+
+   def __str__(self):
+      return 'Circle(%s, %s)' % (self.c, self.r)
 
    def intersect(self, shape):
       if type(shape) == Point or type(shape) == Line:
@@ -145,17 +171,13 @@ class Circle:
             return []
 
          rr = shape.r*shape.r
-         k = (self.r*self.r - rr + dd) / (2 * l)
+         k = (self.r*self.r - rr + dd) / (2 * dd)
          cx = self.c.x + k * dx
          cy = self.c.y + k * dy
-         newc = Point(cx, cy)
-         if newc == self.c:
-            line = Line(self.c, Point(-dy, dx))
-         else:
-            line = Line(newc, self.c)
-            line.v = Point(-dy / l, dx / l)
+         line = Line(Point(cx, cy, '(computed)'), Point(cx-dy, cy+dx, '(computed)'))
          i1 = line.intersect(self)
          for p in i1:
+            p.proof = '%s ^ %s' % (self.name, shape.name)
             dx = shape.c.x - p.x
             dy = shape.c.y - p.y
             if abs(dx*dx + dy*dy - rr) > EPSILON:
@@ -168,18 +190,18 @@ def point(p, shapes, points):
    return shapes, union(points, {p})
 
 def line(p1, p2, shapes, points):
-   intersections = set()
+   intersections = {p1, p2}
    l = Line(p1, p2)
    for s in shapes:
       intersections = union(intersections, {p for p in s.intersect(l) if type(p) == Point})
-   return union(shapes, {l}), union(intersections, union(points, {p1, p2}))
+   return union(shapes, {l}), union(intersections, points)
 
 def circle(c, r, shapes, points):
-   intersections = set()
+   intersections = {c, r}
    circle = Circle(c, r)
    for s in shapes:
       intersections = union(intersections, {p for p in s.intersect(circle) if type(p) == Point})
-   return union(shapes, {circle}), union(intersections, union(points, {c, r}))
+   return union(shapes, {circle}), union(intersections, points)
 
 
 def intersection(p, q):
@@ -204,8 +226,10 @@ def union(p, q):
    return r
 
 def solve(shapes, points, goal, steps):
-   if steps == 1:
-      return intersection(goal, shapes) == shapes
+   if steps == 0:
+      if intersection(goal, shapes) == goal:
+         return shapes, points
+      return False
 
    plist = list(points)
    for i in range(len(plist)):
@@ -231,26 +255,62 @@ def solve(shapes, points, goal, steps):
                return solution
    return False
 
-r = Point(1,0)
-c = Circle(Point(0,0), r)
+def print_solution(solution):
+   shapes, points = solution
+   for p in points:
+      print('%s = %s' % (p.name, p.proof))
 
-p1 = Point(cos(2*pi/3), sin(2*pi/3))
-p2 = Point(cos(-2*pi/3), sin(-2*pi/3))
+   for s in shapes:
+      print('%s = %s' % (s.name, s.proof))
 
-p3 = Point(cos(4*pi/5), sin(4*pi/5))
+r = Point(1,0, '(given)')
+c = Circle(Point(0,0, '(not given)'), r)
+c.proof = '(given)'
 
-solution = solve({c}, {r, p3}, {Line(r, p2)}, 5)
+p1 = Point(cos(2*pi/3), sin(2*pi/3), '(goal)')
+p2 = Point(cos(-2*pi/3), sin(-2*pi/3), '(goal)')
+
+p3 = Point(cos(4*pi/5), sin(4*pi/5), '(arbitrary)')
+
+solution = solve({c}, {r, p3}, {Line(r, p2)}, 4)
 
 if solution:
    print('found! %s' % (solution,))
 else:
    print('not found...')
 
-p4 = Point(cos(-2*pi/3), sin(-2*pi/2) - 1)
-p5 = Point(cos(2*pi/3), sin(2*pi/3) + 1)
+solution = solve({c}, {r, p3}, {Line(p1, p2)}, 4)
 
-p6 = Point(2*cos(2*pi/3)+1, 0)
-c1 = Circle(Point(p6.x-1, 0), p6)
+if solution:
+   print_solution(solution)
+else:
+   print('not found...')
+
+c1 = Circle(r, p3)
+c2 = Circle(p3, r)
+pp1, pp2 = c1.intersect(c)
+pp3, pp4 = c2.intersect(c)
+pp5, pp6 = c2.intersect(c1)
+print('p3: %s\npp1: %s\npp2: %s\npp3: %s\npp4: %s\npp5: %s\npp6: %s' % (p3, pp1, pp2, pp3, pp4, pp5, pp6))
+
+#
+# p3: <-0.8090169943749473, 0.5877852522924732>
+# pp1: <-0.8090169943749472, 0.5877852522924735>
+# pp2: <-0.8090169943749472, -0.5877852522924735> <- ...
+# pp3: <1.0000000000000004, 0>    -- 9.43689570931383e-16>
+# pp4: <0.3090169943749464, -0.9510565162951543>  <- ...
+# pp5: <0.6045284632676539, 1.8605472991527119>
+# pp6: <-0.41354545764260064, -1.272762046860239> <- ...
+#
+c3 = Circle(pp6, pp2)
+pp7, pp8 = c3.intersect(c2)
+print('%s, %s: %s' % (Line(r, pp7), Line(r, pp8), Line(pp8, r) == Line(r, p2)))
+
+p4 = Point(cos(-2*pi/3), sin(-2*pi/2) - 1, '(given)')
+p5 = Point(cos(2*pi/3), sin(2*pi/3) + 1, '(given)')
+
+p6 = Point(2*cos(2*pi/3)+1, 0, '(given)')
+c1 = Circle(Point(p6.x-1, 0, '(given)'), p6)
 
 print('points: %s' % (c.intersect(Line(p5, p4)) == [p1, p2]))
 print('points: %s' % (c.intersect(c1) == [p1, p2]))
