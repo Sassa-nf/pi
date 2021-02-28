8ecb1a9492ca2115594dd0085c025df9d1db94c3
Sassa NF <sassa.nf@gmail.com>
2021-02-28 14:16:45 +0000

    Now moving with sound

---
+++ unity/RocketLab/Assets/Rocket.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource aSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        aSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            aSrc.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            aSrc.Pause();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
