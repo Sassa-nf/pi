132b9c6cb01597dea2594548ae122e3d874fdff8
Sassa NF <sassa.nf@gmail.com>
2021-09-11 10:56:05 +0100

    BNF

--- regex_div3/a.py
+++ regex_div3/a.py
@@ -11,27 +11,28 @@ def div3(s):
    # assert: s[-1] == '1'
    if len(s) < 2:
       return False
-   c = s[-2]
-   s = s[:-2]
-   if c == '1': # assert: was s.endswith('11') - is a multiple of ...01
-      return div3(s)
+   if s[-2] == '1': # assert: s.endswith('11') - is a multiple of ...01
+      return div3(s[:-2])
 
-   return div3_01(s)
+   return div3_01(s[:-2])
 
 def div3_01(s):
    # assert: was s.endswith('01')
+   while s.endswith('11'):
+      s = s[:-2]
+
    if len(s) < 2:
       return False
    # is a multiple of ...11; let's subtract 11 * 11 - that is 1001, and 01 was chopped off already
    if s[-2] == '1':
-      if s[-1] == '0':
-         return div3(s[:-2])
-      return div3_01(s[:-2])
+      # assert: s[-1] == '0'
+      return div3(s[:-2])
+
    # assert: s[-2] == '0'
    if s[-1] == '1':
       # borrow 1
       return borrow1(s[:-2])
-   return borrow1_10(s[:-2])
+   return borrow1_00(s[:-2])
 
 def borrow1(s):
    while s.endswith('00'):
@@ -43,7 +44,7 @@ def borrow1(s):
    # assert: s.endswith('1') - and we borrow it, so it turns into 0
    return div3(s[:-1])
 
-def borrow1_10(s):
+def borrow1_00(s):
    while s.endswith('00'):
       s = s[:-2]
 
@@ -53,9 +54,16 @@ def borrow1_10(s):
    # assert: s.endswith('1') - and we borrow it, so it turns into 0, plus carry
    return div3_01(s[:-1])
 
-
 r = [i for i in range(1000000) if div3(bin(i)[2:]) != (i % 3 == 0)]
 if not r:
    print('ok')
 else:
    print(bin(r[0]))
+
+
+# ok, so BNF for the above is:
+#
+# div3 = '' | div3 0 | div3 11 | div3_01 01
+# div3_01 = div3_01 11 | div3 10 | borrow1 01 | borrow1_00 00
+# borrow1 = borrow1 00 | div3_01 10 | div3 1
+# borrow1_00 = borrow1_00 00 | div3 10 | div3_01 1
