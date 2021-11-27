a92d23a6e28942c8172b005ee079b5972204aedf
Sassa NF <sassa.nf@gmail.com>
2021-11-27 13:23:49 +0000

    Single-threaded is slower, for an unknown reason - but fact4_1 is faster, so we do save on multiplications

--- fibo/a.java
+++ fibo/a.java
@@ -33,26 +33,30 @@ public class a {
       }
 
       long t0 = System.nanoTime();
-      for(int i = 0; i < 10; i++) {
+//      for(int i = 0; i < 10; i++) {
          new Fact(2 * 1024 * 1024).invoke();
-      }
+//      }
 
       long t1 = System.nanoTime();
-      for(int i = 0; i < 10; i++) {
+//      for(int i = 0; i < 10; i++) {
          new Fact2(2 * 1024 * 1024).invoke();
-      }
+//      }
       long t2 = System.nanoTime();
-      for(int i = 0; i < 10; i++) {
+//      for(int i = 0; i < 10; i++) {
         fact3(2 * 1024 * 1024);
-      }
+//      }
       long t3 = System.nanoTime();
-      for(int i = 0; i < 10; i++) {
+//      for(int i = 0; i < 10; i++) {
         fact4(2 * 1024 * 1024);
-      }
+//      }
       long t4 = System.nanoTime();
+//      for(int i = 0; i < 10; i++) {
+        fact4_1(2 * 1024 * 1024);
+//      }
+      long t5 = System.nanoTime();
 
-      System.out.printf("Fact straightforward: %.3f\nFact multiply equally sized nums: %.3f\nFact multiply only odd: %.3f\nFact multiply only odd-2: %.3f\n%s\n",
-                        (t1 - t0) / 1e9, (t2 - t1) / 1e9, (t3 - t2) / 1e9, (t4 - t3) / 1e9, new Fact(20000).invoke().equals(fact4(20000)));
+      System.out.printf("Fact straightforward: %.3f\nFact multiply equally sized nums: %.3f\nFact multiply only odd: %.3f\nFact multiply only odd-2: %.3f\nFact multiply only odd-2, single-threaded: %.3f\n%s\n",
+                        (t1 - t0) / 1e9, (t2 - t1) / 1e9, (t3 - t2) / 1e9, (t4 - t3) / 1e9, (t5 - t4) / 1e9, new Fact(20000).invoke().equals(fact4(20000)));
    }
 
    public static BigInteger fact3(int n) {
@@ -99,6 +103,47 @@ public class a {
       }
 
       return forks(forks, 0, forks.length, 1).invoke().shiftLeft(shifts);
+   }
+
+   public static BigInteger fact4_1(int n) {
+      BigInteger res = BigInteger.ONE;
+      if (n == 0) {
+         return res;
+      }
+
+      int[] sz = new int[32 - Integer.numberOfLeadingZeros(n)];
+      for(int i = sz.length; i-- > 0; n >>= 1) {
+         sz[i] = n;
+      }
+      int ds = 1;
+
+      ForkJoinTask<BigInteger> one = new Fact4(1, 1);
+      int b = sz.length - 1;
+      int shifts = b;
+
+      ForkJoinTask<BigInteger>[] forks = new ForkJoinTask[sz.length-1];
+
+      for(int i = 1; i < sz.length; i++) {
+         int pow = b;
+
+         b -= 1;
+         int from = sz[i-1];
+         int to = sz[i];
+         from = (from & 1) == 1? from + 2: from + 1; // from is an odd number greater than the end of the previous range
+         to = (to & 1) == 1? to + 1: to;
+         ds += (to + 1 - from) >> 1; // how many digits will be computed
+         shifts += ds * b; // given where we are, these are all even - what power of 2 is skipped
+
+         forks[i-1] = from > to ? one: new Fact4(from , to);
+      }
+
+      BigInteger prev = BigInteger.ONE;
+      for(ForkJoinTask<BigInteger> f: forks) {
+         prev = prev.multiply(f.invoke());
+         res = res.multiply(prev);
+      }
+
+      return res.shiftLeft(shifts);
    }
 
    static ForkJoinTask<BigInteger> forks(ForkJoinTask<BigInteger>[] fs, int from, int to, int step) {
