a6c3826723e81971ca826d2df8fb2726d0601685
Sassa NF <sassa.nf@gmail.com>
2021-11-28 15:18:31 +0000

    Better threaded roll-up

--- fibo/a.java
+++ fibo/a.java
@@ -31,6 +31,13 @@ public class a {
             return;
          }
       }
+      for(int i = 0; i < bis.length; i++) {
+         BigInteger bi = fact5(10000 + i);
+         if (!bi.equals(bis[i])) {
+            System.out.println("Oops..." + bi + " != " + bis[i]);
+            return;
+         }
+      }
 
       long t0 = System.nanoTime();
 //      for(int i = 0; i < 10; i++) {
@@ -51,12 +58,27 @@ public class a {
 //      }
       long t4 = System.nanoTime();
 //      for(int i = 0; i < 10; i++) {
-        fact4_1(2 * 1024 * 1024);
+//        fact4_0(2 * 1024 * 1024);
 //      }
       long t5 = System.nanoTime();
+//      for(int i = 0; i < 10; i++) {
+        fact4_1(2 * 1024 * 1024);
+//      }
+      long t6 = System.nanoTime();
+//      for(int i = 0; i < 10; i++) {
+        fact5(2 * 1024 * 1024);
+//      }
+      long t7 = System.nanoTime();
 
-      System.out.printf("Fact straightforward: %.3f\nFact multiply equally sized nums: %.3f\nFact multiply only odd: %.3f\nFact multiply only odd-2: %.3f\nFact multiply only odd-2, single-threaded: %.3f\n%s\n",
-                        (t1 - t0) / 1e9, (t2 - t1) / 1e9, (t3 - t2) / 1e9, (t4 - t3) / 1e9, (t5 - t4) / 1e9, new Fact(20000).invoke().equals(fact4(20000)));
+      System.out.printf("Fact straightforward: %.3f\nFact multiply equally sized nums: %.3f\n" +
+                        "Fact multiply only odd: %.3f\nFact multiply only odd, and only once: %.3f\n" +
+                        "Fact multiply only odd, only once, and roll up single-threaded: %.3f\n" +
+                        "Fact multiply only odd, only once, and roll up single-threaded: %.3f\n" +
+                        "Fact multiply only odd, and only once, better threaded: %.3f\n" +
+                        "%s\n",
+                        (t1 - t0) / 1e9, (t2 - t1) / 1e9, (t3 - t2) / 1e9, (t4 - t3) / 1e9,
+                        (t5 - t4) / 1e9, (t6 - t5) / 1e9, (t7 - t6) / 1e9,
+                        new Fact(20000).invoke().equals(fact4(20000)));
    }
 
    public static BigInteger fact3(int n) {
@@ -103,6 +125,64 @@ public class a {
       }
 
       return forks(forks, 0, forks.length, 1).invoke().shiftLeft(shifts);
+   }
+
+   public static BigInteger fact4_0(int n) {
+      BigInteger res = BigInteger.ONE;
+      if (n == 0) {
+         return res;
+      }
+
+      int[] sz = new int[32 - Integer.numberOfLeadingZeros(n)];
+      for(int i = sz.length; i-- > 0; n >>= 1) {
+         sz[i] = n;
+      }
+      int ds = 1;
+
+      int b = sz.length - 1;
+      int shifts = b;
+
+      ForkJoinTask<BigInteger>[] forks = new ForkJoinTask[sz.length - 1];
+      ForkJoinTask<BigInteger> last = null;
+
+      if (sz.length > 1) {
+         b--;
+         ds += sz[1] < 3? 0: 1;
+         shifts += ds * b;
+
+         last = sz[1] < 3? new Fact4(1, 1): new Fact4(3, sz[1]);
+         forks[0] = last;
+      }
+
+      for(int i = 2; i < sz.length; i++) {
+         b--;
+         int from = sz[i-1];
+         int to = sz[i];
+         from = (from & 1) == 1? from + 2: from + 1; // from is an odd number greater than the end of the previous range
+         to = (to & 1) == 1? to + 1: to;
+         ds += (to + 1 - from) >> 1; // how many digits will be computed
+         shifts += ds * b; // given where we are, these are all even - what power of 2 is skipped
+
+         ForkJoinTask<BigInteger> prev = last;
+         Fact4 f = new Fact4(from , to);
+         last = ForkJoinTask.adapt(() -> {
+            prev.fork();
+            return f.invoke();
+         });
+         forks[i-1] = last;
+      }
+
+      if (last != null) {
+         last.fork();
+      }
+
+      BigInteger prev = BigInteger.ONE;
+      for(ForkJoinTask<BigInteger> f: forks) {
+         prev = prev.multiply(f.join());
+         res = res.multiply(prev);
+      }
+
+      return res.shiftLeft(shifts);
    }
 
    public static BigInteger fact4_1(int n) {
@@ -157,6 +237,55 @@ public class a {
          left.fork();
          return right.invoke().multiply(left.join());
       });
+   }
+
+   public static BigInteger fact5(int n) {
+      BigInteger res = BigInteger.ONE;
+      if (n < 2) {
+         return res;
+      }
+
+      int[] sz = new int[32 - Integer.numberOfLeadingZeros(n)];
+      for(int i = sz.length; i-- > 0; n >>= 1) {
+         sz[i] = n;
+      }
+      int ds = 1;
+
+      Fact4 one = new Fact4(1, 1);
+      int b = sz.length - 1;
+      int shifts = b;
+
+      Fact4[] forks = new Fact4[sz.length-1];
+
+      for(int i = 1; i < sz.length; i++) {
+         int pow = b;
+
+         b -= 1;
+         int from = sz[i-1];
+         int to = sz[i];
+         from = (from & 1) == 1? from + 2: from + 1; // from is an odd number greater than the end of the previous range
+         to = (to & 1) == 1? to + 1: to;
+         ds += (to + 1 - from) >> 1; // how many digits will be computed
+         shifts += ds * b; // given where we are, these are all even - what power of 2 is skipped
+
+         forks[i-1] = from > to ? one: new Fact4(from , to);
+      }
+
+      return forks5(forks, forks.length - 1)[0].shiftLeft(shifts);
+   }
+
+   private static BigInteger[] forks5(Fact4[] forks, int idx) {
+      if (idx == 0) {
+         BigInteger bi = forks[0].invoke();
+         return new BigInteger[]{bi, bi};
+      }
+
+      ForkJoinTask<BigInteger[]> left = ForkJoinTask.adapt(() -> forks5(forks, idx - 1)).fork();
+      BigInteger right = forks[idx].invoke();
+      BigInteger[] res = left.join();
+      res[1] = res[1].multiply(right);
+      res[0] = res[0].multiply(res[1]);
+      return res;
    }
 
    static class Fact extends RecursiveTask<BigInteger> {
